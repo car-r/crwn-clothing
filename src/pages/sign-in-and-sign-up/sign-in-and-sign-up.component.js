3e085159c8dd114521fbb2c6a025866f4cfd9082
car-r <carson.ri@protonmail.com>
2021-08-28 22:44:53 -0700

    sign in component and custom button component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './sign-in-and-sign-up.styles.scss'
+
+const SignInandSignUpPage = () => (
+    <h1 className='sign-in-and-sign-up'>< SignIn/></h1>
+)
+
+export default SignInandSignUpPage
